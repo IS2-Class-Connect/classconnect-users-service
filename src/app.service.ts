eb8affd974d0c324459ddf49f839622778b635f7
Sol Urbano <surbano@fi.uba.ar>
2025-04-06 13:46:43 -0300

    fix tests

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,16 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { IPrismaService } from '.././prisma/prisma.interface';
-import { IConfig } from './config/config.interface';
+
 
 @Injectable()
 export class AppService {
-  constructor(
-    private readonly prismaService: IPrismaService,
-    private readonly configService: IConfig,
-  ) {}
-
   getHello(): string {
     return 'Hello World!';
   }
-
 }
