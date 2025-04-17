2145a55414583dfed682b1d04c5eb71fb007bd0f
Sol Urbano <surbano@fi.uba.ar>
2025-04-17 03:01:07 -0300

    tests: add e2e tests

--- src/testing/e2e.spec.ts
+++ src/testing/e2e.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
-import { UserModule } from '..//modules/user.module'; // Ajustá path
+import { UserModule } from '../modules/user.module'; 
 import { PrismaService } from '../../prisma/prisma.service';
 
 describe('UserController (e2e)', () => {
   let app: INestApplication;
   let prisma: PrismaService;
+  let newUser;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -18,27 +19,76 @@ describe('UserController (e2e)', () => {
 
     prisma = app.get<PrismaService>(PrismaService);
     await app.init();
-  });
 
-  afterAll(async () => {
-    await prisma.prisma.user.deleteMany(); // limpia usuarios de test
-    await app.close();
-  });
+    await prisma.prisma.user.deleteMany();
 
-  it('POST /users - should create a new user from Google login', async () => {
-    const newUser = {
-      uuid: 'eJwvOiUz6YaFSJz9cKkLMNNtMFg20',
+    newUser = {
+      uuid: 'eJwvOiUz6YaFSJz9cKkLMNNtMFg21',
       email: 'emailUser@gmail.com',
       name: 'Username',
       urlProfilePhoto: 'https://firebasestorage.googleapis.com/v0/profile_picture_user.jpg',
       provider: 'google.com'
     };
+  });
 
+  afterAll(async () => {
+    await prisma.prisma.user.deleteMany();
+    await app.close();
+  });
+
+  it('POST /users - should create a new user', async () => {
     const response = await request(app.getHttpServer())
       .post('/users')
       .send(newUser)
       .expect(201);
 
     expect(response.body).toMatchObject(newUser);
+  });
+
+  it('PATCH /users/:uuid/location - should update the location of the user', async () => {
+    const updatedLocation = { latitude: 40.7128, longitude: -74.0060 }; 
+
+    const response = await request(app.getHttpServer())
+      .patch(`/users/${newUser.uuid}/location`)
+      .send(updatedLocation)
+      .expect(200);
+
+    expect(response.body.latitude).toBe(updatedLocation.latitude);
+    expect(response.body.longitude).toBe(updatedLocation.longitude);
+  });
+
+  it('PATCH /users/:email/failed-attempts - should increment failed attempts for the user', async () => {
+    const response = await request(app.getHttpServer())
+      .patch(`/users/${newUser.email}/failed-attempts`)
+      .expect(200);
+
+    expect(response.body.failedAttempts).toBeGreaterThan(0);
+  });
+
+  it('GET /users/:email/check-lock-status - should check the lock status of the user', async () => {
+    const response = await request(app.getHttpServer())
+      .get(`/users/${newUser.email}/check-lock-status`)
+      .expect(200);
+
+    expect(response.body.message).toBe('Account is not locked');
+    expect(response.body.isLocked).toBe(0);
+    expect(response.body.lockedDate).toBeNull();
+  });
+
+  it('GET /users/:uuid - should get a user by uuid', async () => {
+    const response = await request(app.getHttpServer())
+      .get(`/users/${newUser.uuid}`)
+      .expect(200);
+
+    expect(response.body).toMatchObject(newUser);
+  });
+
+  it('GET /users/:uuid - should return 404 if user not found', async () => {
+    const nonExistentUuid = 'non-existent-uuid';
+    const response = await request(app.getHttpServer())
+      .get(`/users/${nonExistentUuid}`)
+      .expect(404);
+
+    expect(response.body.message).toBe('User not found');
   });
 });
