bb90a0df961af54b1c0d62c890a30b683e135d16
Sol Urbano <surbano@fi.uba.ar>
2025-05-20 11:04:11 -0300

    fix: migrations users

---
+++ prisma/migrations/20250520140045_add_tables/migration.sql
@@ -0,0 +1,44 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "uuid" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "urlProfilePhoto" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "latitude" DOUBLE PRECISION,
+    "longitude" DOUBLE PRECISION,
+    "failedAttempts" INTEGER NOT NULL DEFAULT 0,
+    "lastFailedAt" TIMESTAMP(3),
+    "lockUntil" TIMESTAMP(3),
+    "accountLocked" BOOLEAN NOT NULL DEFAULT false,
+    "accountLockedByAdmins" BOOLEAN NOT NULL DEFAULT false,
+    "description" TEXT NOT NULL DEFAULT '',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "lastSession" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("uuid")
+);
+
+-- CreateTable
+CREATE TABLE "UnknownQuestions" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "question" TEXT NOT NULL,
+
+    CONSTRAINT "UnknownQuestions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Feedback" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "answer" TEXT NOT NULL,
+    "comment_feedback" TEXT NOT NULL,
+    "rating" INTEGER NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Feedback_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
